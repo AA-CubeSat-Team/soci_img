457256d4ae5daa72ec9e664c9fd4794bf22c47ce
Haomin Yu <yuhaomin1996@gmail.com>
2019-05-03 16:19:57 -0700

    Added new test

---
+++ Arduino_Test/Serial_Communication_Test/Serial_Communication_Test.ino
@@ -0,0 +1,34 @@
+// Compiler Linkage
+extern "C" {
+  #include <stdbool.h>
+  #include "commandSet.h"
+}
+// Pin Declaration
+static const int Reset = 7;
+
+/*
+ * Simple Test to assess whether the serial communication
+ * is working properly
+ * (Designed for Arduino Uno or similar hardware)
+ * NOTE: It is highly recommended that the Reset pin
+ *       is connected, although it is optional
+ * 
+ * Author: Haomin Yu
+ */
+void setup() {
+  Serial.begin(9600, SERIAL_8N1);
+  pinMode(Reset, OUTPUT);
+  digitalWrite(Reset, LOW);
+  delay(5);
+  digitalWrite(Reset, HIGH);
+  delay(5);
+  Serial.write(syncCommand, sizeof(syncCommand));
+  delay(1000);
+  while(Serial.available()) {
+    Serial.print(Serial.read());
+  }
+}
+
+
+void loop() {
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # soci_img
 Repository for the Imaging Team
+
+## Arduino_Test
+```
+Directory for uCamIII tests on Arduino Uno or related hardware
+```
