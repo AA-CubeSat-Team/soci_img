f9572735bec693789a2ecb4026b503afe28c375a
Haomin Yu <yuhaomin1996@gmail.com>
2019-05-08 16:51:41 -0700

    Data transmission checkpoint

--- Arduino_Test_2/commandSet.ino
+++ Arduino_Test_2/commandSet.ino
@@ -22,7 +22,7 @@ bool initializeCamera() {
   bool ackReceived = false;
   do {
     sendSyncCommand();
-    ackReceived = receiveAckCommand();
+    ackReceived = receiveAckCommand(0x0D);
     delay(5 + syncAttempts++);
   } while((syncAttempts < MAX_SYNC_ATTEMPTS) && !ackReceived);
   return syncAttempts < MAX_SYNC_ATTEMPTS;
@@ -77,9 +77,10 @@ void sendAckSyncCommand() {
 
 /*
  * Attempts to receive a ACK command through serial
+ * that has the matching 'commandID'
  * (Returns false if fails)
  */
-bool receiveAckCommand() {
+bool receiveAckCommand(byte commandID) {
   bool isAckCommand = true;
   byte incoming = 0;
   // Checking if first byte is 0xAA
@@ -90,7 +91,7 @@ bool receiveAckCommand() {
   isAckCommand = isAckCommand && (incoming == 0x0E);
   // Checking if third byte is 0x0D
   incoming = SoftSer.read();
-  isAckCommand = isAckCommand && (incoming == 0x0D);
+  isAckCommand = isAckCommand && (incoming == commandID);
   // Throwing away fourth byte (Debugging byte)
   incoming = SoftSer.read();
   // Checking if fifth byte is 0x00
@@ -104,11 +105,64 @@ bool receiveAckCommand() {
 
 /*
  * Takes a JPEG snapshot picture with 640 x 480 resolution
+ * (Returns whether the process was successful)
  */
-void takePictureJPEG_640_480() {
+static const unsigned short MAX_INIT_ATTEMPTS     = 10;
+static const unsigned short MAX_SET_SIZE_ATTEMPTS = 10;
+static const unsigned short MAX_SNAPSHOT_ATTEMPTS = 10;
+bool takePictureJPEG_640_480() {
   Serial.println("====================");
-  SoftSer.write(initJpegVgaCommand, sizeof(initJpegVgaCommand));
-  delay(1);
+  bool successful = false;
+  unsigned short attempts = 0;
+  // Initializing with INITIAL command with JPEG and VGA settings
+  do {
+    delay(10);
+    SoftSer.write(initJpegVgaCommand, sizeof(initJpegVgaCommand));
+    if(receiveAckCommand(0x01)) {
+      Serial.println("* uCamIII has acknowledged the INITIAL command");
+      successful = true;
+    }
+    else {
+      Serial.println("uCamIII failed to acknowledge the INITIAL command");
+      successful = false;
+    }
+    attempts++;
+  } while ((attempts < MAX_INIT_ATTEMPTS) && !successful);
+  // Setting package size to 512 bytes
+  successful = false;
+  attempts   = 0;
+  do {
+    delay(10);
+    SoftSer.write(setPackageSize512Bytes, sizeof(setPackageSize512Bytes));
+    if(receiveAckCommand(0x06)) {
+      Serial.println("* uCamIII has set the package size");
+      successful = true;
+    }
+    else {
+      Serial.println("uCamIII failed to set package size");
+      successful = false;
+    }
+    attempts++;
+  } while ((attempts < MAX_SET_SIZE_ATTEMPTS) && !successful);
+  // Takes a SNAPSHOT for a compressed picture
+  successful = false;
+  attempts   = 0;
+  do {
+    delay(10);
+    SoftSer.write(snapshotCompressedPicture, sizeof(snapshotCompressedPicture));
+    if(receiveAckCommand(0x05)) {
+      Serial.println("* uCamIII has the snapshot command");
+      successful = true;
+    }
+    else {
+      Serial.println("uCamIII failed to receive snapshot command");
+      successful = false;
+    }
+    attempts++;
+  } while ((attempts < MAX_SNAPSHOT_ATTEMPTS) && !successful);
+  // GET PICTURE command for snapshot picture
+  
   
   Serial.println("====================");
+  return true;
 }
