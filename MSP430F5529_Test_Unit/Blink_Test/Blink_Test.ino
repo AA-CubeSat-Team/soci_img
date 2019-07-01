848c9f12b14906eace052559c38a7484d6270699
Haomin Yu <yuhaomin1996@gmail.com>
2019-07-01 14:06:00 -0700

    Updated documentation

--- MSP430F5529_Test_Unit/Blink_Test/Blink_Test.ino
+++ MSP430F5529_Test_Unit/Blink_Test/Blink_Test.ino
@@ -3,18 +3,18 @@
  * Makes the LED blink at a one second rate 
  * (Tests board and programmer setup)
  */
- 
-#define LED RED_LED
-//#define LED GREEN_LED
   
 void setup() {
-  pinMode(LED, OUTPUT);     
+  pinMode(RED_LED, OUTPUT);    
+  pinMode(GREEN_LED, OUTPUT); 
 }
 
 
 void loop() {
-  digitalWrite(LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
   delay(1000);
-  digitalWrite(LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
   delay(1000);
 }
