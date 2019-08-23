878a293ddde846ee9c2093d3b504073b7334d4a2
Haomin Yu <yuhaomin1996@gmail.com>
2019-08-22 21:53:48 -0700

    Implement stage transmit of data for external device

--- src/sdReadWrite.ino
+++ src/sdReadWrite.ino
@@ -4,18 +4,29 @@
  */
 
 /**
- * Attempts to read the file named 'fileName' from the
- * SD card and transmit the data contained to the external
- * device
+ * Attempts to transmit the data within 'file' from the
+ * SD card and to the external device
+ * For every ACK the system receives, a package is sent
+ * to the external device.
+ * Note that the last package might not be full
  * (Returns true if successful; False otherwise)
  */
-bool sdReadAndTransmit(String fileName) {
-  File toRead = SD.open(fileName);
-  if(toRead) {
-    while(toRead.available()) {
-      Serial.write(toRead.read());
+bool sdReadAndTransmit(File file) {
+  if (file) {
+    unsigned int fileSize = file.size();
+    unsigned int fullPackages = fileSize / EXTERNAL_PACKAGE_SIZE;
+    unsigned int remainingBytes = fileSize % EXTERNAL_PACKAGE_SIZE;
+    for(int i = 0; i < fullPackages; i++) {
+      while((Serial.available() == 0) && (Serial.read() != ACK)) {}
+      for(int bytes = 0; bytes < EXTERNAL_PACKAGE_SIZE; bytes++) {
+        Serial.write(file.read());
+      }
     }
-    toRead.close();
+    while((Serial.available() == 0) && (Serial.read() != ACK)) {}
+    for(int bytes = 0; bytes < remainingBytes; bytes++) {
+      Serial.write(file.read());
+    }
+    file.close();
     return true;
   }
   return false;
