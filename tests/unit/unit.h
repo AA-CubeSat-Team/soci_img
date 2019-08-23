889fa729e779fd97f39ae196bbfa1515301eef6b
Haomin Yu <yuhaomin1996@gmail.com>
2019-08-22 18:28:12 -0700

    add header file

---
+++ tests/unit/unit.h
@@ -0,0 +1,37 @@
+/*
+ * Declarations for functions declared in unit.ino
+ * 
+ * Author: Haomin Yu
+ */
+
+#ifndef __UNIT_H__
+#define __UNIT_H__
+
+/* Compiler Linkage */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Maximum number of images allowed */
+static const unsigned int IMAGES_COUNT = 5;
+
+/* Commands that can be tested */
+static const byte TAKE_PICTURE       = 0x00;
+static const byte GET_THUMBNAIL_SIZE = 0x01;
+static const byte GET_PICTURE_SIZE   = 0x02;
+static const byte GET_THUMBNAIL      = 0x03;
+static const byte GET_PICTURE        = 0x04;
+static const byte SET_CONTRAST       = 0x05;
+static const byte SET_BRIGTHNESS     = 0x06;
+static const byte SET_EXPOSURE       = 0x07;
+static const byte SET_SLEEP_TIME     = 0x08;
+
+/* Possible responses */
+static const byte NAK = 0x00;
+static const byte ACK = 0x01;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* __UNIT_H__ */
--- tests/unit/unit.ino
+++ tests/unit/unit.ino
@@ -1,9 +1,16 @@
+
+/**
+ * Tests the functionality of the IMG software from the point
+ * of view of an external device. (Built for v1.0.0)
+ * Intended to be uploaded to an arduino Uno or similar device
+ * 
+ * Author: Haomin Yu
+ */
+#include "unit.h"
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
