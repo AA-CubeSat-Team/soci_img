27ad0af96497abe4eb4e9c3b163390323c98a067
Haomin Yu <yuhaomin1996@gmail.com>
2019-08-24 20:50:24 -0700

    Fix logical error in sdReadAndTransmit(File)

--- src/SDReadWrite.ino
+++ src/SDReadWrite.ino
@@ -12,17 +12,17 @@
  * (Returns true if successful; False otherwise)
  */
 bool sdReadAndTransmit(File file) {
-  if (file) {
+  if(file) {
     unsigned int fileSize = file.size();
     unsigned int fullPackages = fileSize / EXTERNAL_PACKAGE_SIZE;
     unsigned int remainingBytes = fileSize % EXTERNAL_PACKAGE_SIZE;
     for(int i = 0; i < fullPackages; i++) {
-      while((Serial.available() == 0) && (Serial.read() != ACK)) {}
+      while(Serial.available() == 0 || Serial.read() != ACK) {}
       for(int bytes = 0; bytes < EXTERNAL_PACKAGE_SIZE; bytes++) {
         Serial.write(file.read());
       }
     }
-    while((Serial.available() == 0) && (Serial.read() != ACK)) {}
+    while(Serial.available() == 0 || Serial.read() != ACK) {}
     for(int bytes = 0; bytes < remainingBytes; bytes++) {
       Serial.write(file.read());
     }
